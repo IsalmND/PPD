00241c518ebd88b8549c1b3449d37b81b3a95f59
IsalmND <118760939+IsalmND@users.noreply.github.com>
2025-12-09 11:34:21 +0100

    Add files via upload

---
+++ tokens.js
@@ -0,0 +1,18 @@
+// status can be "online", "idle", "dnd", or "invisible" or "offline"
+export default [
+    {
+        channelId: "1428416620929482845",
+        serverId: "1394750866648727662",
+        token: process.env.token1,
+        selfDeaf: false,
+        autoReconnect: {
+            enabled: true,
+            delay: 5, // ثواني
+            maxRetries: 5,
+        },
+        presence: {
+            status: "idle",
+        },
+        selfMute: true,
+    },
+];
