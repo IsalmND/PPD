04da388a741c8c3b797c23238a7afdf832b19d21
IsalmND <118760939+IsalmND@users.noreply.github.com>
2025-12-14 18:40:43 +0100

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -11,7 +11,7 @@ export default [
             maxRetries: 5,
         },
         presence: {
-            status: "idle",
+            status: "online",
         },
         selfMute: true,
     },
